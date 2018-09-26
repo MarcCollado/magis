bdf6588da9c69182f6926b87d0b3848e739a60b3
Marc Collado <marc@collado.io>
2018-09-26 18:28:55 +0200

    The button of a card is an independent component

---
+++ src/components/CardButton.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { fakeAsbestos } from '../styles/colors';
+
+const CardButton = ({ children }) => (
+  <Container>
+    {children}
+  </Container>
+);
+
+const Container = styled.div`
+  border: 1px solid ${fakeAsbestos}22;
+  border-radius: 0.25em;
+  display: flex;
+  align-items: center;
+  padding: 0.5em 0em;
+  text-align: center;
+  width: 80%;
+`;
+
+
+CardButton.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default CardButton;
