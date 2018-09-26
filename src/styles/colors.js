65b73e35da9d38a5e79009d736b4ed92c453e18f
Marc Collado <marc@collado.io>
2018-09-26 18:30:41 +0200

    Create a file to serve colors across the project

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+const pitchDark = '#2A2E2E';
+const fakeAsbestos = '#525555';
+const plainTurquoise = '#1ABC9C';
+const bianchiGreen = '#C8EFE8';
+
+export {
+  pitchDark,
+  fakeAsbestos,
+  plainTurquoise,
+  bianchiGreen,
+};
