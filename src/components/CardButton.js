a9f93405a00d41fab294b158390213c137a13b5e
Marc Collado <marc@collado.io>
2018-09-27 19:53:12 +0200

    Add support for onClick events

--- src/components/CardButton.js
+++ src/components/CardButton.js
@@ -4,24 +4,32 @@ import styled from 'styled-components';
 
 import { fakeAsbestos } from '../styles/colors';
 
-const CardButton = ({ children }) => (
-  <Container>
+const CardButton = ({ children, onClick }) => (
+  <Container
+    onClick={onClick}
+  >
     {children}
   </Container>
 );
 
 const Container = styled.div`
+  align-items: center;
   border: 1px solid ${fakeAsbestos}22;
   border-radius: 0.25em;
   display: flex;
-  align-items: center;
-  padding: 0.5em 0em;
+  justify-content: center;
+  margin: 0em 0.5em;
+  padding: 0.5em 1em;
   text-align: center;
-  width: 80%;
+
+  &:hover {
+    border: 1px solid ${fakeAsbestos};
+  }
 `;
 
 CardButton.propTypes = {
   children: PropTypes.object.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default CardButton;
