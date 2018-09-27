77d84d9b1e09d16e2b081cf1ee400f6b4cbfa2df
Marc Collado <marc@collado.io>
2018-09-27 19:53:42 +0200

    Add flex component to CTAs

--- src/components/question/FeedLink.js
+++ src/components/question/FeedLink.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import CardButton from '../CardButton'
+import CardButton from '../CardButton';
 
 export default function FeedLink(props) {
   const { id, status } = props;
 
   return (
-    <CardButton>
-      {status === 'UserWillVote' ?
-        <Link
-          to={`/questions/${id}`}
-          style={{ textDecoration: 'none', width: '100%' }}
-        >
-          Vote
-        </Link> :
-        <Link
-          to={`/questions/${id}/details`}
-          style={{ textDecoration: 'none', width: '100%' }}
-        >
-          Poll Details
-        </Link>
-      }
-    </CardButton>
+    <Container>
+      <CardButton>
+        {status === 'UserWillVote'
+          ? (
+            <Link
+              to={`/questions/${id}`}
+              style={{ textDecoration: 'none', width: '100%' }}
+            >
+            Vote
+            </Link>
+          )
+          : (
+            <Link
+              to={`/questions/${id}/details`}
+              style={{ textDecoration: 'none', width: '100%' }}
+            >
+            Poll Details
+            </Link>
+          )
+        }
+      </CardButton>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 FeedLink.propTypes = {
   // from Question
