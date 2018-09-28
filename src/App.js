379cc383ac80f9fbed298e325f4ce67a8ff18ed1
Marc Collado <marc@collado.io>
2018-09-28 08:04:38 +0200

    Add container flex wrapper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import LoadingBar from 'react-redux-loading-bar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 // relative imports
 import { handleInitialData } from './actions/shared';
 import Nav from './components/Nav';
@@ -29,49 +30,54 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
+        <Container>
           <LoadingBar />
           <Nav />
           <Link to="/add"><AddButton /></Link>
-          <Switch>
-            <Route
-              path="/"
-              exact
-              component={NavTabs}
-            />
-            <Route
-              path="/login"
-              exact
-              component={Login}
-            />
-            <PrivateRoute
-              path="/leaderboard"
-              exact
-              component={Leaderboard}
-            />
-            <PrivateRoute
-              path="/add"
-              exact
-              component={AddQuestion}
-            />
-            <PrivateRoute
-              path="/questions/:id/details"
-              exact
-              component={QuestionPage}
-            />
-            <PrivateRoute
-              path="/questions/:id"
-              exact
-              component={QuestionPage}
-            />
-            <Route
-              component={FourOFour}
-            />
-          </Switch>
-        </div>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                component={NavTabs}
+              />
+              <Route
+                path="/login"
+                exact
+                component={Login}
+              />
+              <PrivateRoute
+                path="/leaderboard"
+                exact
+                component={Leaderboard}
+              />
+              <PrivateRoute
+                path="/add"
+                exact
+                component={AddQuestion}
+              />
+              <PrivateRoute
+                path="/questions/:id/details"
+                exact
+                component={QuestionPage}
+              />
+              <PrivateRoute
+                path="/questions/:id"
+                exact
+                component={QuestionPage}
+              />
+              <Route
+                component={FourOFour}
+              />
+            </Switch>
+        </Container>
       </Router>
     );
   }
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default connect()(App);
