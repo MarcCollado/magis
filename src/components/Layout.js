71c5b37e00e8d6255d4f9aa2381e6bf4bb560880
Marc Collado <marc@collado.io>
2018-09-28 08:04:07 +0200

    Create a Layout responsive component

---
+++ src/components/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// import {
+//   mobile, tablet, desktop,
+// } from '../utils/breakpoints';
+// import {
+//   black, fakeAsbestos, turquoise, eggShell,
+// } from '../utils/colors';
+// import {
+//   Title1, Title2, Title3, BodyText, BodyLink, MetaText,
+// } from '../utils/theme';
+
+const Layout = ({ children }) => (
+  <Container>
+      {children}
+  </Container>
+);
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  padding: 0em 2em;
+  max-width: 480px;
+`;
+
+Layout.propTypes = {};
+
+export default Layout;
