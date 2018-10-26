67b6494bc93f38a5b372641600a3685c52377438
Marc Collado <marc@collado.io>
2018-10-26 17:41:36 +0200

    Add webfont

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -7,42 +7,39 @@ import {
 const Title1 = styled.h1`
   box-shadow: inset 0em -0.5em 0em ${bianchiGreen};
   color: ${fakeAsbestos};
+  font-family: 'Fira Sans', 'sans-serif';
   font-size: 28px;
   font-weight: 700;
   letter-spacing: -1px;
-  margin: 0.5em 0em;
-  padding: 0.125em;
   text-transform: uppercase;
 `;
 
 const Title2 = styled.h2`
   color: ${fakeAsbestos};
+  font-family: 'Fira Sans', 'sans-serif';
   font-size: 24px;
   font-weight: 700;
-  letter-spacing: -1px;
-  margin: 0.125em;
-  padding: 0.125em;
   text-transform: uppercase;
 `;
 
 const Title3 = styled.h3`
   color: ${fakeAsbestos};
+  font-family: 'Fira Sans', 'sans-serif';
   font-size: 20px;
   font-weight: 700;
-  letter-spacing: -1px;
-  margin: 0.125em;
-  padding: 0.125em;
   text-transform: uppercase;
 `;
 
 const BodyText = styled.p`
   color: ${fakeAsbestos};
+  font-family: 'Fira Sans', 'sans-serif';
 `;
 
 const BodyLink = styled.a`
   border-bottom: 0.25em solid ${bianchiGreen};
   box-shadow: inset 0 -0.25em 0 ${bianchiGreen};
   color: inherit;
+  font-family: 'Fira Sans', 'sans-serif';
   text-decoration: none;
   transition: background 0.3s ease;
 
@@ -62,6 +59,7 @@ const BodyLink = styled.a`
 
 const MetaText = styled.p`
   color: ${fakeAsbestos}88;
+  font-family: 'Fira Sans', 'sans-serif';
   font-size: 0.75em;
   margin: 0.25em;
   padding: 0.25em;
