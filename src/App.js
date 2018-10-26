095d81f9ce1f35314ff783fc0b1e500ea7049f99
Marc Collado <marc@collado.io>
2018-10-26 23:18:56 +0200

    Add faPlus icon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import LoadingBar from 'react-redux-loading-bar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 // relative imports
 import { handleInitialData } from './actions/shared';
 import AddQuestion from './components/AddQuestion';
@@ -14,7 +16,9 @@ import Login from './components/Login';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import QuestionPage from './components/QuestionPage';
-import AddButton from './components/ui-library/AddButton';
+import AddButton from './components/AddButton';
+
+library.add(faPlus);
 
 class App extends Component {
   static propTypes = {
@@ -28,47 +32,48 @@ class App extends Component {
   }
 
   render() {
+
     return (
       <Router>
         <Container>
           <LoadingBar />
           <Navbar />
           <Link to="/add"><AddButton /></Link>
-            <Switch>
-              <Route
-                path="/"
-                exact
-                component={HomePage}
-              />
-              <Route
-                path="/login"
-                exact
-                component={Login}
-              />
-              <PrivateRoute
-                path="/leaderboard"
-                exact
-                component={LeaderPage}
-              />
-              <PrivateRoute
-                path="/add"
-                exact
-                component={AddQuestion}
-              />
-              <PrivateRoute
-                path="/questions/:id/details"
-                exact
-                component={QuestionPage}
-              />
-              <PrivateRoute
-                path="/questions/:id"
-                exact
-                component={QuestionPage}
-              />
-              <Route
-                component={FourOFour}
-              />
-            </Switch>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              component={HomePage}
+            />
+            <Route
+              path="/login"
+              exact
+              component={Login}
+            />
+            <PrivateRoute
+              path="/leaderboard"
+              exact
+              component={LeaderPage}
+            />
+            <PrivateRoute
+              path="/add"
+              exact
+              component={AddQuestion}
+            />
+            <PrivateRoute
+              path="/questions/:id/details"
+              exact
+              component={QuestionPage}
+            />
+            <PrivateRoute
+              path="/questions/:id"
+              exact
+              component={QuestionPage}
+            />
+            <Route
+              component={FourOFour}
+            />
+          </Switch>
         </Container>
       </Router>
     );
