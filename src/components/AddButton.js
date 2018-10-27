d2f933a4d276035a7cf44ccb1f5640cda1e5d91f
Marc Collado <marc@collado.io>
2018-10-27 17:12:19 +0200

    Remove unnecessary div wrapper

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -7,9 +6,7 @@ import { coralRed, fakeAsbestos } from '../styles/colors';
 
 const AddButton = () => (
   <Button>
-    <Icon>
-      <FontAwesomeIcon icon="plus" color="white" size="lg"/>
-    </Icon>
+    <FontAwesomeIcon icon="plus" color="white" size="lg"/>
   </Button>
 );
 
@@ -32,10 +29,6 @@ const Button = styled.div`
     background: #E2AFAC;
     box-shadow: ${fakeAsbestos}88 0em 0.5em 1em 0em;
   }
-`;
-
-const Icon = styled.div`
-
 `;
 
 export default AddButton;
