4d28613596f4a4e8b254117e228ea827f5109b2a
Marc Collado <marc@collado.io>
2018-10-29 00:07:38 +0100

    Major component refactor — deprecates material-ui dependencies

--- src/components/FourOFour.js
+++ src/components/FourOFour.js
@@ -1,59 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-// imports from material-ui
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-// styles
-import { FourOFour as styles } from '../styles/styles';
+import styled from 'styled-components';
 
-const FourOFour = ({ classes }) => (
-  <div className={classes.container}>
-    <Typography
-      style={{ marginTop: 20 }}
-      variant="display1"
-    >
-      404
-    </Typography>
-    <Typography
-      style={{ textAlign: 'center', marginTop: 15 }}
-      variant="body1"
-    >
-      {'Yikes... this URL doesn\'t belong to any question 😞'}
+import Button from './Button';
+import Layout from './Layout';
+import { fakeAsbestos } from '../styles/colors';
+import { Title1, BodyText, MetaText } from '../styles/typography';
 
-    </Typography>
-    <div
-      style={{ textAlign: 'center', marginTop: 15 }}
-    >
-      <img
-        src="/minions.jpg"
-        alt="minions"
-        style={{ width: '80%' }}
-      />
-    </div>
-    <Typography
-      style={{ marginTop: 10, textAlign: 'center' }}
-      variant="caption"
-    >
-      {'You can either keep staring at these Minions or go back home and have fun voting more questions 🎉'}
-    </Typography>
-
-    <Link to="/" style={{ textDecoration: 'none' }}>
-      <Button
-        style={{ marginTop: 20 }}
-        variant="outlined"
-        className={classes.button}
-      >
-        {'Go 🔙 Home'}
-      </Button>
-    </Link>
-  </div>
+const FourOFour = () => (
+  <Layout>
+    <Title1>
+      {`404`}
+    </Title1>
+    <BodyText>
+      {`Yikes... this URL doesn\'t belong to any question 😞`}
+    </BodyText>
+    <img
+      src="/minions.jpg"
+      alt="minions"
+      style={{ width: '80%' }}
+    />
+    <MetaText>
+      {`You can either keep staring at these cute Minions or go back home and have fun voting more polls 🎉`}
+    </MetaText>
+    <StyledLink to="/">
+      <Button>{'👈 Back Home'}</Button>
+    </StyledLink>
+  </Layout>
 );
 
-FourOFour.propTypes = {
-  // from material-ui
-  classes: PropTypes.object.isRequired,
-};
+const StyledLink = styled(Link)`
+  color: ${fakeAsbestos};
+  text-decoration: none;
 
-export default withStyles(styles)(FourOFour);
+  &:visited, :hover, :active {
+    color: ${fakeAsbestos};
+  }
+`;
+
+export default FourOFour;
