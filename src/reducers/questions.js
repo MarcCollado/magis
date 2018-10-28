d7e2cdab1ff99fcb76714eeab37a7facb4361f0b
Marc Collado <marc@collado.io>
2018-10-28 13:32:59 +0100

    Use spread operator to add a new vote in the case REGISTER_VOTE

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -23,7 +23,7 @@ export default function questions(state = {}, action) {
           ...state[action.qid],
           [action.answer]: {
             ...state[action.qid][action.answer],
-            votes: state[action.qid][action.answer].votes.concat([action.authedUser]),
+            votes: [...state[action.qid][action.answer].votes, action.authedUser]
           },
         },
       };
