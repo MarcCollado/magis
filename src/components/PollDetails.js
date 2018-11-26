6242c525a83a0bbc975269e3a5a697473ad1bc7b
Marc Collado <marc@collado.io>
2018-11-26 23:41:08 +0100

    Pull poll from state instead of Router

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -17,11 +17,12 @@ import { Title1, BodyText, MetaText } from '../styles/typography';
 const PollDetails = ({
   location,
   match,
+  poll,
   realName,
   imageURL,
   errorPage,
 }) => {
-  const { poll, voted } = location.state;
+  const { voted } = location.state;
   const { id } = match.params;
 
   // sanity check the id of the URL matches the one on the poll
@@ -33,9 +34,7 @@ const PollDetails = ({
 
   return (
     <Layout>
-      <Title1>
-        Poll Details
-      </Title1>
+      <Title1>Poll Details</Title1>
       <BodyText>
         {voted === 1
           ? `👇 You voted for the option one`
@@ -93,11 +92,13 @@ function mapStateToProps({ polls, users, authUser }, { match }) {
       errorPage,
     };
   }
+  const poll = polls[match.params.id];
   const userName = polls[match.params.id].author;
   const realName = authUser === userName ? 'you' : users[userName].name;
   const imageURL = users[userName].avatarURL;
   const errorPage = false;
   return {
+    poll,
     realName,
     imageURL,
     errorPage,
