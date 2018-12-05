76db07bdd3ffface003b80f9c56700065ff28c82
Marc Collado <marc@collado.io>
2018-12-05 22:33:46 +0100

    Update api endpoints to get application ready for deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,38 +1,30 @@
 import { database } from './firebase';
-import { _getUsers } from './_DATA';
 
 export function getInitialData() {
   // fetch seed data from Firebase
-  return database.ref('seed').once('value')
+  return database.ref().once('value')
     .then((snapshot) => ({
       users: snapshot.val().users, polls: snapshot.val().polls,
-    }));
-}
-
-export function getAuthUsers() {
-  return database.ref('seed').once('value')
-    .then((snapshot) => ({
-      users: snapshot.val().users,
     }));
 }
 
 export function createUserToDB(userData) {
   const { userID } = userData;
   const updates = {};
-  updates[`/seed/users/${userID}`] = userData;
+  updates[`/users/${userID}`] = userData;
   return database.ref().update(updates);
 }
 
 export function createPollToDB(pollData) {
   const { pollID } = pollData;
   const updates = {};
-  updates[`/seed/polls/${pollID}`] = pollData;
+  updates[`/polls/${pollID}`] = pollData;
   return database.ref().update(updates);
 }
 
 export function registerVoteToDB(voteData) {
   const { pollID, vote } = voteData;
   const updates = {};
-  updates[`/seed/polls/${pollID}/votes`] = vote;
+  updates[`/polls/${pollID}/votes`] = vote;
   return database.ref().update(updates);
 }
