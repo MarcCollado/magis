62fbd5d39536ce017f961fdac2bfcb0c317c29db
Marc Collado <marc@collado.io>
2018-12-05 20:34:37 +0100

    Remove method to register vote in the user state

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { GET_USERS, CREATE_USER, REGISTER_VOTE } from '../actions/actionTypes';
+import { GET_USERS, CREATE_USER } from '../actions/actionTypes';
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -11,17 +11,6 @@ export default function users(state = {}, action) {
       return {
         ...state,
         [action.user.userID]: action.user,
-      };
-    case REGISTER_VOTE:
-      return {
-        ...state,
-        [action.authedUser]: {
-          ...state[action.authedUser],
-          votes: {
-            ...state[action.authedUser].votes,
-            [action.pollId]: action.vote,
-          },
-        },
       };
     default:
       return state;
