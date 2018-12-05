72e3d33834fbd37b78839b924efdb0fa49bde91e
Marc Collado <marc@collado.io>
2018-12-05 16:14:17 +0100

    getUsers named import

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,10 @@
 import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 import { getPolls } from './polls';
-import getUsers from './users';
+import { getUsers } from './users';
 import { getInitialData } from '../utils/api';
 
+// TODO: this method has to get the data from Firebase
 export default function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
