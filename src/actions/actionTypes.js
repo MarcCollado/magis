cb11da0aa97ee1c73afe4e009183821898ab0d6b
Marc Collado <marc@collado.io>
2018-12-06 10:05:01 +0100

    Refactor file structure

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,10 +1,12 @@
-// auth
+// AUTH
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
-// polls
+
+// POLLS
 export const GET_POLLS = 'GET_POLLS';
 export const CREATE_POLL = 'CREATE_POLL';
 export const REGISTER_VOTE = 'REGISTER_VOTE';
-// users
+
+// USERS
 export const GET_USERS = 'GET_USERS';
 export const CREATE_USER = 'CREATE_USER';
