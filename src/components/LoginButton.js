4a1e14a4a2a3429bbc0489b1b547a61397995b8b
Marc Collado <marc@collado.io>
2018-12-06 10:07:11 +0100

    Create dedicated login button for OAuth

---
+++ src/components/LoginButton.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { fakeAsbestos } from '../styles/colors';
+import { BodyText } from '../styles/typography';
+
+const LoginButton = ({
+  children, disabled, onClick, provider,
+}) => {
+  const colorMatrix = {
+    Github: '#333',
+    Twitter: '#38A1F3',
+  };
+
+  return (
+    <Button
+      color={colorMatrix[provider]}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <StyledBody theme={provider}>
+        {children}
+      </StyledBody>
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  background: ${(props) => props.color};
+  border: 2px solid ${(props) => props.color};
+  border-radius: 0.25em;
+  color: ${(props) => props.color};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+`;
+
+const StyledBody = styled(BodyText)`
+  color: ${(props) => (props.provider === 'Github' || 'Twitter' ? '#FFF' : fakeAsbestos)};
+`;
+
+LoginButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  provider: PropTypes.oneOf(['Github', 'Twitter']).isRequired,
+};
+
+export default LoginButton;
