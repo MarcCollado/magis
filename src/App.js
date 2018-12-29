e18eee528ce3b0b8a096fc3a79e5a12e09778572
Marc Collado <marc@collado.io>
2018-12-29 17:52:37 +0100

    Wrap App into a ThemeProvider component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlus, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -20,10 +20,25 @@ import Navbar from './components/Navbar';
 import PollDetails from './components/PollDetails';
 import PrivateRoute from './components/PrivateRoute';
 
+import {
+  electricBlue, coralRed, bianchiGreen, plainPurple, seriousYellow, fakeAsbestos,
+} from './styles/colors';
 import { database } from './utils/firebase';
 import { seedPolls, seedUsers } from './utils/helpers';
 
 library.add(faPlus, faUserCircle);
+
+const theme = {
+  electricBlue,
+  coralRed,
+  bianchiGreen,
+  plainPurple,
+  seriousYellow,
+  fakeAsbestos,
+  minWidth: '320px',
+  maxWidth: '480px',
+  boxShadow88: `${fakeAsbestos}88 0em 0.125em 0.25em 0em`,
+};
 
 class App extends Component {
   componentDidMount() {
@@ -47,42 +62,44 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Container>
-          <LoadingBar />
-          <Navbar />
-          {/* <button onClick={this.seedFirebase}>Reset Firebase</button> */}
-          <Link to="/create"><CreatePollButton /></Link>
-          <Switch>
-            <Route
-              path="/"
-              exact
-              component={HomePage}
-            />
-            <Route
-              path="/login"
-              exact
-              component={LoginPage}
-            />
-            <PrivateRoute
-              path="/leaderboard"
-              exact
-              component={LeaderPage}
-            />
-            <PrivateRoute
-              path="/create"
-              exact
-              component={CreatePollPage}
-            />
-            <PrivateRoute
-              path="/polls/:id/details"
-              exact
-              component={PollDetails}
-            />
-            <Route
-              component={FourOFour}
-            />
-          </Switch>
-        </Container>
+        <ThemeProvider theme={theme}>
+          <Container>
+            <LoadingBar />
+            <Navbar />
+            {/* <button onClick={this.seedFirebase}>Reset Firebase</button> */}
+            <Link to="/create"><CreatePollButton /></Link>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                component={HomePage}
+              />
+              <Route
+                path="/login"
+                exact
+                component={LoginPage}
+              />
+              <PrivateRoute
+                path="/leaderboard"
+                exact
+                component={LeaderPage}
+              />
+              <PrivateRoute
+                path="/create"
+                exact
+                component={CreatePollPage}
+              />
+              <PrivateRoute
+                path="/polls/:id/details"
+                exact
+                component={PollDetails}
+              />
+              <Route
+                component={FourOFour}
+              />
+            </Switch>
+          </Container>
+        </ThemeProvider>
       </Router>
     );
   }
